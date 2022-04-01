ab1c31625f6d7ec6362579055940c2146345f9fd
Owner <owner@Owners-MacBook-Pro.local>
2022-04-01 17:05:13 -0500

    added user routes

--- dlc_server/index.js
+++ dlc_server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
-
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+
+app.use('/users', userRoutes)
 
 const PORT = 3000
 
---
+++ dlc_server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { createUser, updateUser, deleteUser, getUser, getUsers } from '../controllers/userController.js'
+
+const router = express.Router()
+
+router.get('/', getUsers)
+router.get('/:id', getUser)
+router.post('/', createUser)
+router.patch('/:id', updateUser)
+router.delete('/:id', deleteUser)
